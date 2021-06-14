5493a9deb2517044eb91577fa2c856e8c6dac4ab
James Woolfenden <James.Woolfenden@gmail.com>
2021-06-14 13:34:55 +0100

    refresh

--- google_service_account.service.tf
+++ google_service_account.service.tf
@@ -1,4 +1,5 @@
 resource "google_service_account" "service" {
+  # checkov:skip=CKV2_GCP_3: ADD REASON
   account_id   = var.service["id"]
   project      = var.project
   display_name = var.service["display_name"]
